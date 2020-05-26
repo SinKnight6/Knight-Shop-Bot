2f14f2560ae3d6f805035d92fbdb831550687b3b
SinKnight6 <sinnersquadhq@gmail.com>
2020-05-26 02:01:20 -0500

    first commit

--- index.js
+++ index.js
@@ -35,34 +35,34 @@ bot.on("guildMemberAdd", async member => {
 
                          HERE ARE THE RULES:
 
-    :smiley: 1. Be cool, kind, and civil. Treat all members with respect and express your thoughts in a constructive manner.
+:smiley: 1. Be cool, kind, and civil. Treat all members with respect and express your thoughts in a constructive manner.
 
-    :abc: 2. Use English only. Communicate in English, but be considerate of all languages.
+:abc: 2. Use English only. Communicate in English, but be considerate of all languages.
    
-   :card_index: 3. Use appropriate nicknames. Avoid special characters, emoji, obscenities, and impersonation.
+:card_index: 3. Use appropriate nicknames. Avoid special characters, emoji, obscenities, and impersonation.
    
-   :incoming_envelope:  4. Do not spam. Avoid excessive messages, images, formatting, emoji, commands, @mentions and NEVER Dm advertise, It is against TOS and we will not allow our members to do break TOS
+:incoming_envelope:  4. Do not spam. Avoid excessive messages, images, formatting, emoji, commands, @mentions and NEVER Dm advertise, It is against TOS and we will not allow our members to do break TOS
    
-   :no_bell:  5. Do not spam Staff. Respect their time, they are people too.
+:no_bell:  5. Do not spam Staff. Respect their time, they are people too.
    
-   :loud_sound:  6. No self-promotion or advertisements. This includes unsolicited references and links to other social media, servers, communities, and services in chat or direct messages.
+:loud_sound:  6. No self-promotion or advertisements. This includes unsolicited references and links to other social media, servers, communities, and services in chat or direct messages.
    
-   :shield:  7. No personal information. Protect your privacy and the privacy of others.
+:shield:  7. No personal information. Protect your privacy and the privacy of others.
    
-   :head_bandage:  8. No harassment, abuse, or bullying. We have zero-tolerance for harming others.
+:head_bandage:  8. No harassment, abuse, or bullying. We have zero-tolerance for harming others.
    
-   :anger_right:  9. No racist, sexist, anti-LGBTQ+, or otherwise offensive content. We have zero-tolerance for hate speech.
+:anger_right:  9. No racist, sexist, anti-LGBTQ+, or otherwise offensive content. We have zero-tolerance for hate speech.
    
-   :classical_building:  10. No political or religious topics. These complex subjects result in controversial and offensive posts.
+:classical_building:  10. No political or religious topics. These complex subjects result in controversial and offensive posts.
    
-   :rotating_light:  11. No piracy, NSFW, or otherwise suspicious content. We do not condone illegal or suspicious discussions and activity.
+:rotating_light:  11. No piracy, NSFW, or otherwise suspicious content. We do not condone illegal or suspicious discussions and activity.
    
-   :thinking:  12. Rules are subject to common sense. These rules are not comprehensive and use of loopholes to violate the spirit of these rules is subject to enforcement.
+:thinking:  12. Rules are subject to common sense. These rules are not comprehensive and use of loopholes to violate the spirit of these rules is subject to enforcement.
    
-   :scroll:  13. Discord Terms of Service and Community Guidelines apply. 
-   You must be at least 13 years old to use Discord, and abide by all other terms and guidelines. 
+:scroll:  13. Discord Terms of Service and Community Guidelines apply. 
+You must be at least 13 years old to use Discord, and abide by all other terms and guidelines. 
    
-   :key: 14. Ones you’re done reading the rules please type, \`?verify\` in order to unlock server.`)
+:key: 14. Ones you’re done reading the rules please type, \`?verify\` in order to unlock server.`)
 
 
 bot.on("message", async message => {
