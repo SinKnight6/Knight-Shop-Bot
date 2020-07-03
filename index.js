dfb981171f3f57c3cca7a6f42df1fc54c8917f53
SinKnight6 <sinnersquadhq@gmail.com>
2020-07-02 21:12:14 -0500

    seventh commit

--- index.js
+++ index.js
@@ -8,23 +8,52 @@ const PREFIX = '$';
 
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on("ready", async () => {
-    console.log(`${bot.user.username} is online!`)
+bot.on("ready", () => {
+    var testChannel = bot.channels.cache.find(channel => channel.id === '687713445008769143');
 
-    let statuses = [
-        `${bot.guilds.cache.size} Servers`,
-        "!help",
-        `over ${bot.users.cache.size} users!`,
-        "BIG CHUNGUS",
-        "YOUR MOM",
-        "GTAV",
-        "Over Knight-Shop",
-        "Payments"
-    ]
-    setInterval(function() {
-        let status = statuses[Math.floor(Math.random() * statuses.length)]
-        bot.user.setActivity(status, {type: "WATCHING"});
-    }, 5000)
+    console.log(`${bot.user.username} is online!`)
+    
+    setInterval(() => {
+      const embed = new Discord.MessageEmbed();
+      embed.setTitle("Welcome to our server!");
+      embed.setColor(000000);
+      embed.setDescription(`HERE ARE THE RULES:
+    
+      :smiley: 1. Be cool, kind, and civil. Treat all members with respect and express your thoughts in a constructive manner.
+      
+      :abc: 2. Use English only. Communicate in English, but be considerate of all languages.
+      
+      :card_index: 3. Use appropriate nicknames. Avoid special characters, emoji, obscenities, and impersonation.
+      
+      :incoming_envelope: 4. Do not spam. Avoid excessive messages, images, formatting, emoji, commands, @mentions and NEVER Dm advertise, It is against TOS and we will not allow our members to do break TOS.
+      
+      :no_bell: 5. Do not spam Staff. Respect their time, they are people too and **DO NOT** DM staff for help please use the support ticket or ask for help in the appropiate channel (People who DM staff will just get ignored). 
+      
+      :loud_sound: 6. No self-promotion or advertisements. This includes unsolicited references and links to other social media, servers, communities, and services in chat or direct messages.
+      
+      :shield: 7. No personal information. Protect your privacy and the privacy of others.
+      
+      :head_bandage: 8. No harassment, abuse, or bullying. We have zero-tolerance for harming others.
+      
+      :anger_right: 9. No racist, sexist, anti-LGBTQ+, or otherwise offensive content. We have zero-tolerance for hate speech.
+      
+      :classical_building: 10. No political or religious topics. These complex subjects result in controversial and offensive posts.
+      
+      :rotating_light: 11. Do not post any NSFW in channels not marked as NSFW.
+      
+      :thinking: 12. Rules are subject to common sense. These rules are not comprehensive and use of loopholes to violate the spirit of these rules is subject to enforcement.
+      
+      :scroll: 13. Discord Terms of Service and Community Guidelines apply. 
+      You must be at least 13 years old to use Discord, and abide by all other terms and guidelines. 
+      
+      :key: 14. **Once you’re done reading the rules please REACT to <:Member:726551839155290132> in order to unlock server.**`)
+  
+  testChannel.send(embed)
+  .then(sentMessage => sentMessage.delete({ timeout: 54000000 })
+   .catch(error => {
+    // Hnadler
+  }))
+    }, 54000000);
     
 })
 
