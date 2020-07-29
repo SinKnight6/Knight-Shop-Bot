5272935e1a95e2c6ad0be7b1f586526a2218f287
SinKnight6 <sinnersquadhq@gmail.com>
2020-07-29 17:00:27 -0500

    seventh commit

--- index.js
+++ index.js
@@ -8,12 +8,78 @@ const PREFIX = '$';
 
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on('ready', () => {
-  var testChannel = bot.channels.cache.find(channel => channel.id === '687713445008769143');
+bot.on('ready', () => console.log(`${bot.user.tag} has logged in fucker.`));
 
-  console.log('bot is online!')
+bot.on('message', async message => {
+  if (message.author.bot) return;
+
+  if (message.content.toLowerCase() === 'yeet' && message.channel.id === '695566841291997244'){
+    let embed = new Discord.MessageEmbed()
+    .setColor(000000)
+    .setDescription(` **__Welcome to Knight-Shop!__**
+
+    Hello! You are required to complete this capthca before entering the server
+
+    **Why?** 
+    This is to protect the serveer against targeted attacks using automated user accounts.
+
+    The Capthca:
+
+    \`!verify\``)
+    message.channel.send({embed: embed});
+   };
   
-  setInterval(() => {
+  bot.on('guildMemberAdd', member => {
+    console.log(member.user.tag);
+  });
+
+  if (message.channel.id === '687713445008769143')
+    await message.delete();
+  if (message.content.toLowerCase() === '!verify' && message.channel.id === '687713445008769143')
+  {
+    message.channel.send(`${message.author} Attempting to verify you in **Kight-Shop** <a:Loading:705280596217430019>`)
+  .then(sentMessage => sentMessage.delete({ timeout: 10000 })
+ .catch(error => {
+  // Hnadler
+}))
+.then(() => {
+  message.channel.awaitMessages(response => response.content === '', {
+    max: 1,
+    time: 100,
+    errors: ['time'],
+  })
+  .then((collected) => {
+      message.channel.send(`The collected message was: ${collected.first().content}`);
+    })
+    .catch(() => {
+      let uEmbed6 = new Discord.MessageEmbed()
+    .setTitle('__Welcome__')
+    .setColor(3066993)
+    .setDescription(`You are now Verified, Welcome to our server ${message.author}`)
+    message.channel.send({embed: uEmbed6})
+      .then(sentMessage => sentMessage.delete({ timeout: 60000})
+ .catch(error => {
+    }));
+  });
+});
+    await message.delete().catch(err => console.log(err));
+    const role = message.guild.roles.cache.get('687960444916596886');
+    if(role) {
+      try {
+      setTimeout( async () => {
+      await message.member.roles.add(role); }, 10000)
+      console.log('Role added!');
+    }
+    catch(err) {
+      console.log(err);
+      }
+    }
+  }
+
+  bot.on('message', message => {
+    if(message.author.bot) return;
+
+  if (message.content.toLowerCase() === '^Rules' && message.channel.id === '738151642884472896'){
     const embed = new Discord.MessageEmbed();
     embed.setTitle("Welcome to our server!");
     embed.setColor(000000);
@@ -44,50 +110,10 @@ bot.on('ready', () => {
 :thinking: 12. Rules are subject to common sense. These rules are not comprehensive and use of loopholes to violate the spirit of these rules is subject to enforcement.
     
 :scroll: 13. Discord Terms of Service and Community Guidelines apply. 
-You must be at least 13 years old to use Discord, and abide by all other terms and guidelines.
-
-:key: 14. Ones you’re done reading the rules please type, \`REACT\` to <:Member:726551839155290132> in order to unlock server.`)
-
-testChannel.send(embed)
-.then(sentMessage => sentMessage.delete({ timeout: 25200000})
- .catch(error => {
-  // Hnadler
-}))
-  }, 25200000);
-  
-
-})
-
-bot.on('message', message => {
-  if (message.author.bot)
-  {
-    if(message.embeds)
-    {
-      const embedMsg = message.embeds.find(msg => msg.title === 'Welcome to our server!');
-      if(embedMsg)
-      {
-        message.react('726551839155290132')
-        .then(reaction => console.log('Reacted with' + reaction.emoji.name))
-        .catch(err => console.err);
-      }
-    }
-     return;
-
+You must be at least 13 years old to use Discord, and abide by all other terms and guidelines.`)
+message.channel.send({embed: embed});
   }
-
-  
-});
-
-bot.on('messageReactionAdd', (reaction,user) => {
-  if(user.bot)
-    return;
-
-    var roleName = reaction.emoji.name;
-    var role = reaction.message.guild.roles.cache.find(role => role.name.toLowerCase() === roleName.toLowerCase());
-    var member = reaction.message.guild.members.cache.find(member => member.id === user.id);
-    member.roles.add(role.id).then(member => {
-      console.log("Added" + member.user.username + "to a role.");
-    }).catch(err => console.error);
+  })
 
 
 
